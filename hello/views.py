f889430719e982ed5f221569e7bf7c5c023ad442
Sadvitha <84315215+Sadvi3103461@users.noreply.github.com>
2022-04-05 17:29:35 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, MindTree ---   With te help of all the steps Please create Azure Web Application and attatch all the screen shots ")
+    return HttpResponse("Hello, MindTree ---    attatch all the screen shots ")
